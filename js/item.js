5076f20fd9f8645af0c030e91016376dc1918182
BabyDiscoPanda <patineur@hotmail.fr>
2023-05-20 13:59:17 +0200

    V0.2.1 : amélioration de l'UI

--- js/item.js
+++ js/item.js
@@ -1,7 +1,7 @@
 // crée le tableau d'objet :
 
     let LongName;
-    let Names; 
+    let Names;
 
 function loadItem(){
     LongName ="Mom's Ring > Glass Eye > Stye > Decap Attack > Gello > Ghost Bombs > Hemoptysis > IBS > Hypercoagulation > Spindown Dice > Anima Sola > TMTRAINER > Everything Jar > Keeper's Box > Keeper's Kin > Keeper's Sack (Keeper's Elbow) > Sumptorium > Lemegeton > Flip > Bag of Crafting > Suplex! > Stapler > Supper > Abyss > Dark Arts > Berserk! > Esau Jr. > Vengeful Spirit > Isaac's Tomb > Echo Chamber > Azazel's Rage > Twisted Pair > Vanishing Twin > Salvation > Bloody Gust > Heartbreak > The Swarm > Sanguine Bond > Sacred Orb > Belly Jelly > Glitched Crown > Inner Child > Friend Finder > Soul Locket > Jar of Wisps > Hungry Soul > Bone Spurs > Worm Friend > Lil Portal > Montezuma's Revenge > Lil Abaddon > C Section > Astral Projection > Empty Heart > Cracked Orb > Spirit Shackles > Redemption > A Pound of Flesh > Candy Heart > Options? > Sausage > Dad's Note > Strawman > Guppy's Eye > Binge Eater > Tooth and Nail > Quints > Card Reading > Tropicamide > Giant Cell > Vasculitis > Spin to Win > False PHD > Vade Retro > Cube Baby > Star of Bethlehem > Plum Flute > Fruity Plum > 4.5 Volt > Brimstone Bombs > Tinytoma > Consolation Prize > Revelation > Magic Skin > Akeldama > Urn of Souls > Yuck Heart > Eraser > Knockout Drops > R Key > Stitches > Purgatory > Dogma > Evil Charm > Meat Cleaver > Bot Fly > Death Certificate > Knife Piece 2 > Knife Piece 1 > Mega Mush > Booster Pack > Sharp Key > Genesis > Red Stew > Birthright > Rotten Tomato > Lodestone > Bird's Eye > Lil Dumpy > Blood Bombs > Lost Soul > Larynx > Bird Cage > Eternal D6 > Freezer Baby > Boiled Baby > Ocular Rift > The Scooper > Mom's Bracelet > Battery Pack > Member Card > Act of Contrition > Eye Drops > Voodoo Head > Pluto > Neptunus > Uranus > Saturnus > Jupiter > Mars > Terra > Venus > Mercurius > Luna > Sol > The Stairway > Alabaster Box > Book of Virtues > Rocket in a Jar > Wavy Cap > Psy Fly > Red Key > Spirit Sword > Free Lemonade > Damocles > Dirty Mind > The Intruder > Monstrance > Immaculate Heart > Eye of the Occult > Orphan Socks > Playdough Cookie > Blood Oath > Divine Intervention > Paschal Candle > Dream Catcher > Blood Puppy > A Bar of Soap > Nancy Bombs > Rock Bottom > Almond Milk > It Hurts > 120 Volt > Eye Sore > Fortune Cookie > Sulfur > Golden Razor > 2Spooky > Mucormycosis > Mom's Shovel > Broken Shovel (Boss Rush) > Broken Shovel (Basement I) > Brittle Bones > Jaw Bone > Divorce Papers > Dad's Ring > Book of the Dead > Pointy Rib > Hallowed Ground > Slipped Rib > Marrow > Flat Stone > Mystery Egg > Marbles > Lil Spewer > Sacrificial Altar > Blanket > Schoolbag > Trisagion > Lachryphagy > Haemolacria > Death's List > Pop! > Angelic Prism > Mr. ME! > 7 Seals > Leprosy > Technology Zero > Moving Box > Telekinesis > (good item for me to) Coupon > Jumper Cables > Lil Delirium > Buddy In A Box > Fast Bombs > Sprinkler > Mystery Gift > Broken Modem > Bozo > Black Hole > Angry Fly > Delirious > Bloodshot Eye > Mom's Razor > Sharp Straw > Backstabber > Poke Go > Brown Nugget > Little Horn > Large Zit > Greed's Gullet > Sack of Sacks > Eucharist > Duality > Camo Undies > Euthanasia > Ghost Pepper > Jacob's Ladder > Adrenaline > YO LISTEN! > Acid Baby > Eden's Soul > D Infinity > Metronome > Potato Peeler > Dull Razor > Crooked Penny > Wait What? > Mama Mega! > Clicker > Dataminer > Compost > Smelter > Pause > Void > D1 > Plan C > Broken Glass Cannon > Big Chubby > King Baby > Lil Monstro > Hushy > Depression > Shade > Finger! > Contagion > Analog Stick > Glyph of Balance > Sulfuric Acid > Eye of Belial > Parasitoid > Glaucoma > Sinus Infection > Belly Button > Cone Head > Midnight Snack > Dad's Lost Coin > Polydactyly > Compound Fracture > Varicose Veins > Tarot Cloth > Eye of Greed > Metal Plate > Shard of Glass > Linger Bean > Dead Tooth > Dog Tooth > Lead Pencil > Apple! > Dark Prince's Crown > Mega Blast > Kidney Stone > Mom's Box > Binky > D7 > Milk! > Lil Loki > Jar of Flies > My Shadow > Glitter Bombs > Multidimensional Baby > Papa Fly > Head of the Keeper > PJs > Mine Crafter > Obsessed Fan > Night Light > Sack Head > Circle of Protection > Glowing Hour Glass > Kidney Bean > Black Powder > Teleport 2.0 > Fruit Cake > Succubus > Deep Pockets > Crown of Light > More Options > Immaculate Conception > Cambion Conception > Lusty Blood > Evil Eye > Empty Vessel > Athame > Purity > D8 > GB Bug > Farting Baby > Spider Mod > Chaos > Explosivo > Spear of Destiny > Maw of the Void > God's Flesh > Tractor Beam > Ventricle Razor > Tech X > Marked > Serpent's Kiss > Zodiac > Betrayal > Seraphim > Rune Bag > Key Bum > Censer > D12 > Bumbo > Lil Gurdy > Tear Detonator > Friendly Ball > Eden's Blessing > Pay to Play > Pupula Duplex > No. 2 > Bursting Sack > Restock > Host Hat > Holy Light > Dead Eye > Charged Baby > Curse of the Tower > Mr. Dolly > Continuum > Epiphora > Sticky Bombs > Scatter Bombs > Lost Fly > Friend Zone > Sworn Protector > Lil Chest > Fate's Reward > Incubus > 8 Inch Nails > The Wiz > Box of Friends > Car Battery > Mom's Pearls > Crack Jacks > Bomber Boy > Glass Cannon > Mega Bean > Toxic Shock > Wooden Nickel > Placebo > Diplopia > A Snack > Synthoil > Match Book > Latch Key > Blue Cap > Torn Photo > Caffeine Pill > Safety Pin > The Boomerang > Broken Watch > Dead Onion > The Soul > The Body > The Mind > Lazarus' Rags > Godhead > Soy Milk > The Ludovico Technique > The Negative > The Polaroid > Breath of Life > Scissors > Undefined > Isaac's Tears > Mongo Baby > Samson's Chains > ???'s Only Friend > Cain's Other Eye > Gemini > Mysterious Liquid > Cursed Eye > Strange Attractor > Thunder Thighs > Holy Mantle > Maggy's Bow > Judas' Shadow > Eve's Mascara > Pisces > Aquarius > Capricorn > Sagittarius > Scorpio > Libra > Virgo > Leo > Cancer > Aries > Taurus > Unicorn Stump > Pandora's Box > Converter > Magic Fingers > Butter Bean > Head of Krampus > Satanic Bible > Flush! > The Jar > Red Candle > Box of Spiders > Book of Secrets > Blank Card > D10 > D4 > D100 > How To Jump > Punching Bag > Sissy Longlegs > Big Fan > Dark Bum > Lil Haunt > Isaac's Heart > Lil Brimstone > Best Bud > Bob's Brain > BBF > Mystery Sack > Leech > Headless Baby > Rotten Baby > Robo-Baby 2.0 > Juicy Sack > Dry Baby > Smart Fly > Clear Rune > Missing Page 2 > Proptosis > Black Candle > Dark Matter > Missing No. > Fire Mind > Hot Bombs > Screw > Blood Clot > Magic Scab > Little Baggy > Starter Deck > Bogo Bombs > There's Options > Hive Mind > BFFS! > Blue Map > 20/20 > Tech.5 > Trinity Shield > Infamy > Contract From Below > Experimental Treatment > Key Piece 2 > Key Piece 1 > Death's Touch > E Coli > Infestation 2 > Tiny Planet > Stop Watch > Ball of Tar > Abaddon > Monstro's Lung > Mom's Perfume > Piggy Bank > Black Lotus > Gimpy > Cricket's Body > Pyromaniac > Anti-Gravity > Rubber Cement > Sad Bombs > Old Bandage > Placenta > Mom's Wig > Ceremonial Robes > Goat Head > Anemic > Lost Contact > Guppy's Collar > Spiderbaby > Gnawed Leaf > Butt Bombs > Champion's Belt > Ball of Bandages > Guillotine > Sharp Plug > Fanny Pack > Humbling Bundle > Midas' Touch > Iron Bar > Mom's Eyeshadow > Mom's Key > Box > Jesus Juice > Squeezy > Mom's Coin Purse > Magic 8 Ball > MEAT! > Telepathy For Dummies > 3 Dollar Bill > Pyro > SMB Super Fan > Abel > Guppy's Hair Ball > Blood Rights > Dead Dove > Holy Grail > Toothpicks > Sacred Heart > White Pony > The Black Bean > Fate > Holy Water > Portable Slot > Stem Cells > Dad's Key > Rainbow Baby > Mitre > Sacrificial Dagger > Spider Butt > Daddy Longlegs > Polyphemus > Epic Fetus > Harlequin Baby > D20 > Cat-O-Nine-Tails > The Candle > Ghost Baby > Celtic Cross > Ankh > Crack the Sky > Spirit of the Night > Crystal Ball > Bloody Lust > Habit > The Peeper > Chemical Peel > Mutant Spider > Technology 2 > The Mulligan > Tough Love > Ipecac > Infestation > Notched Axe > Prayer Card > Guppy's Head > Bum Friend > Speed Ball > Scapular > Pageant Boy > Bob's Curse > Mom's Purse > Stigmata > Remote Detonator > Best Friend > IV Bag > Guppy's Tail > Guppy's Paw > A Lump of Coal > Bomb Bag > A Pony > Bucket of Lard > Forever Alone > Forget Me Now > Razor Blade > Bobby-Bomb > Dead Sea Scrolls > Monster Manual/Monster Manuel > Whore of Babylon > Odd Mushroom (flat) > Odd Mushroom (thin) > Blood Bag > Brimstone > Dead Bird > 9-Volt > Ouija Board > Mom's Knife > Demon Baby > Guardian Angel > The Bean > Mom's Contact > Money = Power > The Wafer > The Pinking Shears > Mr. Mega > The D6 > The Parasite > The Common Cold > Mom's Bottle of Pills > The Halo > Little Steven > Little Gish > The Relic > The Book of Sin > Little C.H.A.D. (Little CHAD) > Robo-Baby > Sack of Pennies > The Gamekid > Super Bandage > Spelunker Hat > The Small Rock > Spider Bite > Little Chubby > Loki's Horns > Monstro's Tooth > Deck of Cards > We Need To Go Deeper! > The Nail > Lord of the Pit > Dead Cat > The Pact > The Mark > Book of Revelations > My Little Unicorn > X-Ray Vision > PHD > A Quarter > Cube of Meat > Rosary > Mini Mush > Growth Hormones > Chocolate Milk > Technology > Sister Maggy > The Hourglass > Anarchist Cookbook > Steam Sale > The Battery > Charm of the Vampire > The Ladder > Book of Shadows > Distant Admiration > Lemon Mishap > Mom's Eye > Treasure Map > Magneto > Dr. Fetus > Pentagram > Steven > Shoop Da Whoop! > Cupid's Arrow > Doctor's Remote > Lucky Foot > Yum Heart > Teleport > Bob's Rotten Head > Mom's Pad > Kamikaze > Mom's Bra > Tammy's Head > Mr. Boom > The Poop > The Necronomicon > The Book of Belial > The Bible > Wire Coat Hanger > Mom's Lipstick > Mom's Heels > Mom's Underwear > The Belt > Wooden Spoon > Rotten Meat > Breakfast > Dessert > Dinner > Lunch > The Compass > Transcendence > Boom! > A Dollar > Skeleton Key > Raw Liver > <3 > Roid Rage > The Virus > Magic Mushroom > 1up! > Halo of Flies > Skatole > Brother Bobby > Blood of the Martyr > Number One > My Reflection > Cricket's Head > Spoon Bender > The Inner Eye > The Sad Onion > deletes itself" 
@@ -32,6 +32,7 @@ function Commencer(){
         Item2();
         Item3();
         Item4();
+        document.getElementById("suiv").style.display = "block";
 }
 
 function Avancer(){
@@ -40,9 +41,6 @@ function Avancer(){
     if(compteur < Names.length - 1){
         document.getElementById('Ttest').innerHTML = "";
         document.getElementById('Ttest').innerHTML = `${compteur} : ${Names[compteur]}`;
-    } else {
-        document.getElementById('Ttest').innerHTML = "";
-        document.getElementById('Ttest').innerHTML = `ELEMENT DELETED`;
     }
 }
 
@@ -57,7 +55,7 @@ function Item1(){
 }
 
 function Item2(){
-    if(compteur+2 < Names.length - 2){
+    if(compteur+2 < Names.length - 1){
         document.getElementById('Ttest2').innerHTML = "";
         document.getElementById('Ttest2').innerHTML = `${compteur+2} : ${Names[compteur+2]}`;
     } else {
@@ -67,7 +65,7 @@ function Item2(){
 }
 
 function Item3(){
-    if(compteur+3 < Names.length - 3){
+    if(compteur+3 < Names.length - 1){
         document.getElementById('Ttest3').innerHTML = "";
         document.getElementById('Ttest3').innerHTML = `${compteur+3} : ${Names[compteur+3]}`;
     } else {
@@ -77,7 +75,7 @@ function Item3(){
 }
 
 function Item4(){
-    if(compteur+4 < Names.length - 4){
+    if(compteur+4 < Names.length - 1){
         document.getElementById('Ttest4').innerHTML = "";
         document.getElementById('Ttest4').innerHTML = `${compteur+4} : ${Names[compteur+4]}`;
     } else {
@@ -88,4 +86,10 @@ function Item4(){
 
 function Recherche(){
     compteur  = Names.length-5;
+    document.getElementById('Ttest').innerHTML = `${compteur} : ${Names[compteur]}`;
+    Item1();
+    Item2();
+    Item3();
+    Item4();
+    document.getElementById("suiv").style.display = "block";
 }
